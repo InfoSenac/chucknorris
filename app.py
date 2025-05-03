e302a6ad300b53cf04d438bfae6b2237b12849ea
InfoSenac <125390815+InfoSenac@users.noreply.github.com>
2025-05-02 21:15:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ import requests
 
 st.title('Piadas do Chuck Norris')
 
-api_key = st.secrets['API_KEY']
-
 api_url = 'https://api.api-ninjas.com/v1/chucknorris'
 response = requests.get(
     api_url, 
     headers={
-        'X-Api-Key': api_key
+        'X-Api-Key': st.secrets['API_KEY']
     }
 )
